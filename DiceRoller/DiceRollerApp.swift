b1353f5f24183f1842a36f49d597fa7d1c567c96
Em Pongpat <empongpat@gmail.com>
2025-04-06 23:51:28 +0800

    feat: create Xcode Swift iOS app project

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Pongpat Weesommai on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
