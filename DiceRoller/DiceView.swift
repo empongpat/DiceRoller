1265e292887bd30d13bacb30ad37b67a720d6005
Em Pongpat <empongpat@gmail.com>
2025-04-19 23:59:35 +0800

    feat: add Roll button to random dice number

--- DiceRoller/DiceView.swift
+++ DiceRoller/DiceView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct DiceView: View {
-    var numberOfPips: Int = 1
+    @State private var numberOfPips: Int = 1
     
     var body: some View {
-        Image(systemName: "die.face.\(numberOfPips)")
-            .resizable()
-            .frame(width: 100, height: 100)
+        VStack {
+            Image(systemName: "die.face.\(numberOfPips)")
+                .resizable()
+                .frame(width: 100, height: 100)
+            Button("Roll") {
+                numberOfPips = Int.random(in: 1...6)
+            }
+        }
     }
 }
 
