4fe75a61de1f37db7fb738f91630ff295d7522dc
Em Pongpat <empongpat@gmail.com>
2025-05-10 23:01:26 +0800

    feat: updates content view to show title and 3 dices

--- DiceRoller/ContentView.swift
+++ DiceRoller/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Dice Roller").font(.largeTitle.lowercaseSmallCaps().bold())
+            
+            HStack {
+                ForEach(1...3, id: \.description) { _ in
+                    DiceView()
+                }
+            }
         }
         .padding()
     }
