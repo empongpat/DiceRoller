8f4f391229f6e73030ae7889b0aca097d5176958
Em Pongpat <empongpat@gmail.com>
2025-05-11 23:59:09 +0800

    feat: updates number of dice to be dynamic

--- DiceRoller/ContentView.swift
+++ DiceRoller/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var numberOfDice: Int = 1
     var body: some View {
         VStack {
             Text("Dice Roller").font(.largeTitle.lowercaseSmallCaps().bold())
             
             HStack {
-                ForEach(1...3, id: \.description) { _ in
+                ForEach(1...numberOfDice, id: \.description) { _ in
                     DiceView()
                 }
+            }
+            
+            HStack {
+                Button("Remove Dice") {
+                    numberOfDice -= 1
+                }
+                .disabled(numberOfDice == 1)
+                
+                Button("Add Dice") {
+                    numberOfDice += 1
+                }
+                .disabled(numberOfDice == 3)
             }
         }
         .padding()
